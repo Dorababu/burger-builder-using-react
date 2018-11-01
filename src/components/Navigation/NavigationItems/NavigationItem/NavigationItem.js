2e54a993d01677f33a495e95b2638cec94769cab
Dorababu Chodisetti <dorababu.ch@gmail.com>
2018-11-01 20:05:13 +0530

    Building Navigation.

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classses from './NavigationItem.css'
+
+const navigationItem = (props) => (
+
+    <li><a href=''>Link1</a></li>
+);
+
+export default navigationItem;
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classses from './NavigationItems.css'
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = (props) => (
+
+    <ul>
+        <NavigationItem />
+    </ul>
+);
+
+export default navigationItems;
